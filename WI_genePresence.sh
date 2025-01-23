4309f005a383a7c8afce4576b735ee692a8ba49c
loconn13 <loconn13@login03.cm.cluster>
2025-01-22 21:03:52 -0500

    updated scripts to run full analysis on current gff3

--- WI_genePresence.sh
+++ WI_genePresence.sh
@@ -5,7 +5,10 @@
 #SBATCH -p parallel                     # Partition/Queue name
 #SBATCH -t 48:00:00                     # Job walltime/duration (hh:mm:ss)
 #SBATCH -N 1                            # Number of nodes
-#SBATCH -n 24                           # Number of cores
+#SBATCH -c 12                           # Number of cores
+#SBATCH --output=/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/status.oe  
+#SBATCH --error=/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/status.rr 
+
 
 #=================================
 
@@ -16,8 +19,7 @@
 # cd /vast/eande106/data/c_elegans/WI/alignments
 # ls *.bam > /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/raw_data/Ce_WI_allStrainList.txt
 
-# while IFS= read -r strain; do sbatch --export=strain=${strain%.bam} getVariantCounts.sh; done < Ce_WI_allStrainList.txt 1 
-# $1 is the coverage depth for a gene 
+# while IFS= read -r strain; do sbatch --export=strain=${strain%.bam} WI_genePresence.sh; done < ../../raw_data/Ce_WI_allStrainList.txt
 
 # mamba create -n gene_annotation mosdepth 
 source activate gene_annotation
@@ -26,25 +28,128 @@ bams="/vast/eande106/data/c_elegans/WI/alignments"
 raw_data="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/raw_data"
 processed_data="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data"
 
-ref_genome="c_elegans.PRJNA13758.WS283.genome.fa"
-GFF="c_elegans.PRJNA13758.WS283.csq.gff3"
+# cd $processed_data/gene_coverage
+# mkdir -p $strain 
+# cd $strain
+# mosdepth $strain $bams/$strain.bam --by $processed_data/refGenesCoordinates.bed --threads 12 --thresholds 1,2,3,4,5
 
-# strain_genome_cov_file="/data/eande106/eande106/analysis/alignment-nf/20231203-CE/20240213_c_elegans_gatk_ss.tsv" #1618 strains - 13 short.... there are 1631 total
-# WIs_coverage=$processed_data/WIs_mean_genome_cov.csv
+# Assessing presence/absence of genes
+for coverage in 1 2 3 4 5; do
 
-# Make 1kb windows of the reference genome
-bedtools makewindows -g $raw_data/$ref_genome -w 1000 > $processed_data/N2.WS238.1kb_windows.bed
+    echo "WBGeneID,$strain" > $processed_data/gene_status/$strain.${coverage}x.98.genes.csv
 
-# Extract gene coordinates from the gff3
-awk '$3 == "gene" {print $1, $4, $5, $9}' OFS="\t" $GFF | sed 's/ID=gene://;s/;.*//' > $processed_data/refGenesCoordinates.bed
+    zcat $processed_data/gene_coverage/$strain/$strain.thresholds.bed.gz | \
+    awk -v coverage=$coverage -v threshold=0.98 '
+    NR > 1 {
+        gene = $4;                            
+        total_bases = $3 - $2;                
+        bases_covered = $(coverage + 4);     # Select the correct coverage column ($5 for 1x, $6 for 2x..... $9 for 5x)
 
-# Intersect to get genome contents at each gene coordinate
-bedtools intersect $processed_data/N2.WS238.1kb_windows.bed $processed_data/refGenesCoordinates.bed > $processed_data/N2.refGenes.bed
+        if ((bases_covered / total_bases) >= threshold) {
+            print gene ",1";                 # Gene is present
+        } else {
+            print gene ",0";                 # Gene is absent
+        }
+    }' >> $processed_data/gene_status/$strain.${coverage}x.98.genes.csv
+done
 
-# Calculating coverage of each strain for ref genome
-cd $processed_data/gene_coverage
-mosdepth $strain $bams/$strain.bam --by $processed_data/N2.refGenes.bed --threads 24 --thresholds 1,2,3,4,5
+
+# # Concatenate all together to create a genotype matrix
+# awk '{print $1}' 
+# for file in *.1x.genes.csv; 
+#     awk "$1 == $1 of XXX' then paste $2  
+# done
+
+# for file in *.2x.genes.csv; 
+#     paste 
+# done
+
+# for file in *.3x.genes.csv; 
+#     paste
+# done 
+
+# for file in *.4x.genes.csv; 
+#     paste 
+# done
+
+# for file in *.5x.genes.csv; 
+#     paste 
+# done
+
+
+
+
+# Calculating coverage of the binned reference genome
+# cd $processed_data/binned_coverage
+# mosdepth $strain $bams/$strain.bam --by $processed_data/N2.WS238.1kb_windows.bed --threads 12 --thresholds 1,2,3,4,5
 # will output $strain.regions.bed.gz
+# bedtools coverage -a $processed_data/refGenesCoordinates.bed -b $bams/$strain.bam -d > $processed_data/gene_coverage/$strain.refGenesCov.bedtoolsCoverage.bed
+# bedtools coverage -a $processed_data/refGenesCoordinates.bed -b $bams/$strain.bam -hist > $processed_data/gene_coverage/$strain.refGenesCov.bedtoolsCoverageHist.bed
+
+
+
+
+
+# # Intersect to get get the coverage at each gene locus
+# zcat $processed_data/binned_coverage/$strain.regions.bed.gz | \
+# bedtools intersect -a $processed_data/refGenesCoordinates.bed -b stdin -wa -wb > $processed_data/gene_coverage/$strain.refGenesCov.bed
+
+
+
+# Reformatting and calculating fraction of gene that has coverage depth x
+# awk 'BEGIN {
+#     # Print headers
+#     print "CHROM\tSTART\tEND\tGene\tTotal_Bases\tbasesCovered_1x\tFractionCovered_1x\tbasesCovered_2x\tFractionCovered_2x\tbasesCovered_3x\tFractionCovered_3x\tbasesCovered_4x\tFractionCovered_4x\tbasesCovered_5x\tFractionCovered_5x";
+# } 
+# {
+#     gene = $4;
+#     chrom[gene] = $1; # Store chromosome for the gene
+#     start[gene] = $2; 
+#     end[gene] = $3;
+#     covered_1x[gene] += $9;
+#     covered_2x[gene] += $10;
+#     covered_3x[gene] += $11;
+#     covered_4x[gene] += $12;
+#     covered_5x[gene] += $13;
+#     total_bases[gene] += ($3 - $2);
+# } 
+# END {
+#     for (gene in total_bases) {
+#         total = total_bases[gene];
+#         frac_1x = (total > 0) ? covered_1x[gene] / total : 0;
+#         frac_2x = (total > 0) ? covered_2x[gene] / total : 0;
+#         frac_3x = (total > 0) ? covered_3x[gene] / total : 0;
+#         frac_4x = (total > 0) ? covered_4x[gene] / total : 0;
+#         frac_5x = (total > 0) ? covered_5x[gene] / total : 0;
+#         print chrom[gene], start[gene], end[gene], gene, total, covered_1x[gene], frac_1x, covered_2x[gene], frac_2x, covered_3x[gene], frac_3x, covered_4x[gene], frac_4x, covered_5x[gene], frac_5x;
+#     }
+# }' OFS="\t" $processed_data/gene_coverage/$strain.refGenesCov.bedtoolsIntersect.bed > $processed_data/gene_coverage/$strain.CovSummary_refGenesBinnedGenome.tsv
+
+
+
+# awk '
+# BEGIN { OFS="\t"; print "CHROM", "START", "END", "WBGeneID", "total_bases", "1x_covered", "1x_fraction", "2x_covered", "2x_fraction", "3x_covered", "3x_fraction", "4x_covered", "4x_fraction", "5x_covered", "5x_fraction" }
+# {
+#     key = $1 FS $2 FS $3 FS $4
+#     count[key]["total"]++
+#     if ($6 >= 1) count[key]["1x"]++
+#     if ($6 >= 2) count[key]["2x"]++
+#     if ($6 >= 3) count[key]["3x"]++
+#     if ($6 >= 4) count[key]["4x"]++
+#     if ($6 >= 5) count[key]["5x"]++
+# }
+# END {
+#     for (key in count) {
+#         split(key, fields, FS)
+#         total = count[key]["total"]
+#         print fields[1], fields[2], fields[3], fields[4], total, \
+#             count[key]["1x"], count[key]["1x"] / total, \
+#             count[key]["2x"], count[key]["2x"] / total, \
+#             count[key]["3x"], count[key]["3x"] / total, \
+#             count[key]["4x"], count[key]["4x"] / total, \
+#             count[key]["5x"], count[key]["5x"] / total
+#     }
+# }' gene_coverage.tsv > gene_thresholds_summary.tsv
 
 
 
@@ -53,65 +158,8 @@ mosdepth $strain $bams/$strain.bam --by $processed_data/N2.refGenes.bed --thread
 
 
 
-cd $processed_data/gene_coverage
-for WI_bam in $bams/*.bam; do
-    mosdepth --by $processed_data/N2.refGenes.bed --fast-mode --threads 48 ${WI_bam%.bam} $WI_bam #output is ${WI_bam%bam}.regions.bed.gz. Also run without --fast-mode?? 
-done
-
-# CHANGE APPROACH TO BIN THE GENOME into 1kb bins and extract coverage > to a bed 
-# then bedtools intersect of gene.ed with genome_coverage_1kbBIN.bed to get coverage at each gene
-# then look at coverage of WI.bam for the bins and assess 1x, 2x, 3x, 4x, 5x for 80% or more of the bases in that gene and plot number of genes classified as present/absent and best coverage depth parameters is when the #s between two coverages are about the same - where the count stops drastically falling off
-$1 is the coverage depth for a gene 
-
-echo "STRAIN,AVE_COV" > $WIs_coverage
-for coverage in $processed_data/gene_coverage/*.regions.bed.gz; do
-    strain=${coverage%%.*} # Get strain name from filename
-    strain_coverage=$(awk -v strain="$strain" '$1 == strain {print $4}' $strain_genome_cov_file) # Get average genome coverage for the strain
-
-    echo "$strain,$strain_coverage" >> $WIs_coverage
-    
-    # Process gene coverage for this strain - ADD THE STRAIN NAME TO THE HEADER OF EACH FINAL FOR FINAL MERGING
-    zcat $coverage | awk -v strain=$strain -v avg_cov=$strain_coverage '
-        {
-            # Calculate coverage ratio for each gene
-            gene_cov_ratio = $4 / avg_cov
-
-            if (gene_cov_ratio >= 0.8) {   # Criterion that the coverage for a gene must be at least 80% of the average genome-wide coverage for its strain
-                print $1, $2, $3, $4, "1"  # Present 
-            } else {
-                print $1, $2, $3, $4, "0"  # Absent 
-            }
-        }' > $processed_data/gene_status/${strain}.gene_status.$1.tsv
-done
-
-final_matrix=$processed_data/final_matrix/gene_presence_matrix.$1.tsv 
-
-# Aggregate results - each row is a gene, each column is a WI, and then each cell is a PRESENT (1) / ABSENT (0) for that gene in that strain
-paste $processed_data/gene_status/*.gene_status.tsv | awk '{for (i=5; i<=NF; i+=5) printf "%s ", $i; print ""}' >> $final_matrix
 
 
 
 
 
-
-
-
-
-### Write a python script to calculate frequency of a gene across all WIs
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Load presence/absence matrix
-df = pd.read_csv("gene_presence_matrix.tsv", sep="\t", header=None)
-
-# Compute frequency of each gene across strains
-freq = df.mean(axis=1) * 100  # Percentage of strains with the gene
-
-# Classify genes
-df['Category'] = pd.cut(freq, bins=[0, 5, 95, 100], labels=['rare', 'soft_core', 'core'])
-df.loc[freq == 0, 'Category'] = 'Private'
-
-df.to_csv("gene_classification.tsv", sep="\t", index=False)
-
-plot the distrubition of frequency of genes appearing in WSs???? 
-plot a pie chart representing the proportion of each type of gene???
