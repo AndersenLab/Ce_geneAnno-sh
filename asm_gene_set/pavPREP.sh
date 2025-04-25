73c620ee6195e0c4fd9b60f04ddb26efc7f0220f
loconn13 <loconn13@login02.cm.cluster>
2025-04-25 11:22:50 -0400

    updated script

--- asm_gene_set/pavPREP.sh
+++ asm_gene_set/pavPREP.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-#SBATCH -J pavPREP
-#SBATCH -A eande106
-#SBATCH -p parallel
-#SBATCH -t 48:00:00
-#SBATCH -N 1
-#SBATCH -c 24
-#SBATCH --output=/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pavprep.oe  
-#SBATCH --error=/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pavprep.rr 
-
 pav="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/pav"
-assemblies="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/pav/assemblies"
+# strain_dirs=($pav/strain_dirs/*/)
+# strain_dir=${strain_dirs[$SLURM_ARRAY_TASK_ID-1]}
+assemblies="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/raw_data/assemblies/briggsae" ##### REMEMBER TO CHANGE FOR EACH SPECIES
 SLURM="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output"
 
-
-# tail -n +2 $pav/assemblies.tsv | while IFS=$'\t' read -r strain path; do
+# tail -n +2 $pav/assemblies_Cb_0423.tsv | while IFS=$'\t' read -r strain path; do
 #     mkdir -p $pav/strain_dirs/$strain
-#     cp $pav/config.json  $pav/strain_dirs/$strain
+#     cp $pav/config_Cb.json  $pav/strain_dirs/$strain/config.json
 
 #     cd  $pav/strain_dirs/$strain
 #     echo -e "NAME\tHAP_unphased\n$strain\t$path" > $pav/strain_dirs/$strain/assemblies.tsv
     
 #     mkdir -p $pav/strain_dirs/$strain/assemblies
-#     cp $assemblies/${strain}.hifi.inbred.fa $pav/strain_dirs/$strain/assemblies
+#     cp $assemblies/$strain.hifi.inbred.fa $pav/strain_dirs/$strain/assemblies
 # done
 
 for strain_dir in $pav/strain_dirs/*/; do
@@ -32,8 +24,41 @@ for strain_dir in $pav/strain_dirs/*/; do
 
         strain_name=$(basename $strain_dir)
         sbatch --job-name="pav${strain_name}" \
-        --output="$SLURM/pav_${strain_name}.oe" \
-        --error="$SLURM/pav_${strain_name}.rr" \
+        --output="$SLURM/pav_${strain_name}briggsae.oe" \
+        --error="$SLURM/pav_${strain_name}briggsae.rr" \
         /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/Ce_geneAnno-sh/asm_gene_set/pav.sh
     fi 
 done
+
+
+
+
+# for strain_dir in /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/pav/strain_dirs/*/; do
+#     cd $strain_dir
+#     if ! ls *.vcf.gz &>/dev/null; then 
+#         rm -r .snakemake data log results temp
+
+#         strain_name=$(basename $strain_dir)
+#         sbatch --job-name="pav${strain_name}" \
+#         --output="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pav_${strain_name}LATENCY900.oe" \
+#         --error="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pav_${strain_name}LATENCY900.rr" \
+#         /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/Ce_geneAnno-sh/asm_gene_set/pav.sh
+#     fi 
+# done
+
+
+
+# sbatch --job-name="pavXECA2581" --output="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pav_XECA2581_LATENCY900.oe" --error="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pav_XECA2581_LATENCY900.rr" /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/Ce_geneAnno-sh/asm_gene_set/pav.sh
+# sbatch --job-name="pavECA36" --output="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pav_ECA36_LATENCY900.oe" --error="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pav_ECA36_LATENCY900.rr" /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/Ce_geneAnno-sh/asm_gene_set/pav.sh
+# sbatch --job-name="pavECA594" --output="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pav_ECA594_LATENCY900.oe" --error="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/SLURM_output/pav_ECA594_LATENCY900.rr" /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/Ce_geneAnno-sh/asm_gene_set/pav.sh
+
+# cd $strain_dir
+# if ! ls *.vcf.gz &>/dev/null; then 
+#     rm -r .snakemake data log results temp
+
+#     strain_name=$(basename $strain_dir)
+#     sbatch --job-name="${strain_name}LATENCY900" \
+#     --output="$SLURM/${strain_name}LATENCY900.oe" \
+#     --error="$SLURM/${strain_name}LATENCY900.rr" \
+#     /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/scripts/Ce_geneAnno-sh/asm_gene_set/pav.sh
+# fi
