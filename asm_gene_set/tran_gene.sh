333df063a7f3b24898876080023c14f89fd74216
loconn13 <loconn13@login03.cm.cluster>
2025-05-24 13:56:46 -0400

    script to convert transcripts to genes and de-deuplicate genes in an orthogroup

--- asm_gene_set/tran_gene.sh
+++ asm_gene_set/tran_gene.sh
@@ -7,40 +7,39 @@
 #SBATCH -N 1                            # Number of nodes
 #SBATCH -n 8                           # Number of cores
 
-# awk '{print "s/\\<" $1 "\\>/" $2 "/g"}' transcripts_gene_115WI_N2.tsv > tran_gene_key.tsv
-# sed -f tran_gene_key.tsv N0.tsv > N0_genes.tsv
+sed -i 's|transcript_||g' /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/N0_N2_braker_test.tsv
 
-#awk 'BEGIN { FS=OFS="\t" }
-#    NR==FNR { map[$1]=$2; next }  # Read key file: transcript -> gene
-#    {
-#        for (i=1; i<=NF; i++) {
-#            # Replace each comma-separated transcript with the gene(s)
-#            n=split($i, a, /, */)
-#            for (j=1; j<=n; j++) {
-#                a[j] = (a[j] in map ? map[a[j]] : a[j])
-#            }
-#            $i = a[1]
-#            for (j=2; j<=n; j++) {
-#                $i = $i ", " a[j]
-#            }
-#        }
-#        print
-#    }' /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/transcripts_gene_115WI_N2.tsv /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/N0.tsv > /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/N0_0422_genes.tsv
+awk 'BEGIN { FS=OFS="\t" }
+    NR==FNR { map[$1]=$2; next }  # Read key file: transcript -> gene
+    {
+        for (i=1; i<=NF; i++) {
+            # Replace each comma-separated transcript with the gene(s)
+	    n=split($i, a, /, */)
+            for (j=1; j<=n; j++) {
+                a[j] = (a[j] in map ? map[a[j]] : a[j])
+            }
+            $i = a[1]
+            for (j=2; j<=n; j++) {
+                $i = $i ", " a[j]
+            }
+        }
+        print
+    }' /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/transcripts_gene_115WI_N2.tsv /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/N0_N2_braker_test.tsv > /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/N0_N2_braker_genes.tsv
 
-awk -F'\t' 'BEGIN {OFS="\t"}
-NR == 1 {print; next}
-{
-  for (i = 4; i <= NF; i++) {
-    split($i, a, /, */)
-    delete seen
-    out = ""
-    for (j in a) {
-      if (a[j] != "" && !(a[j] in seen)) {
-        seen[a[j]] = 1
-        out = (out ? out ", " : "") a[j]
-      }
-    }
-    $i = out
-  }
-  print
-}' /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/N0_0422_genes.tsv > /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/N0_0422_genes_dedup.tsv
+#awk -F'\t' 'BEGIN {OFS="\t"}
+#NR == 1 {print; next}
+#{
+#  for (i = 4; i <= NF; i++) {
+#    split($i, a, /, */)
+#    delete seen
+#    out = ""
+#    for (j in a) {
+#      if (a[j] != "" && !(a[j] in seen)) {
+#        seen[a[j]] = 1
+#        out = (out ? out ", " : "") a[j]
+#      }
+#    }
+#    $i = out
+#  }
+#  print
+#}' /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/N0_0425_genes.tsv > /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/orthofinder/N0_0425_genes_dedup.tsv
