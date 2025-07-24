d33ba970f5290b785f0953a7bd672ec21d210502
loconn13 <loconn13@login02.cm.cluster>
2025-07-24 12:59:12 -0400

    script to initialize and run BRAKER container. First test on 07/24/2025 and ran in directory /vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/misc/N2_BRAKER_error/container_braker3

---
+++ asm_gene_set/brakerN2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH -J braker3
+#SBATCH -A eande106
+#SBATCH -p parallel
+#SBATCH -t 48:00:00
+#SBATCH -N 1
+#SBATCH -c 24
+
+module load singularity 
+
+input="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/misc/N2_BRAKER_error"
+output="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/misc/N2_BRAKER_error/container_braker3/output"
+#busco="/vast/eande106/projects/Nicolas/WI_PacBio_genomes/annotation/elegans/busco_downloads/lineages"
+container="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/software/containers/loconn13999-braker3_20250724.sif"
+
+singularity exec \
+	--bind $input:/input \
+	--bind $output:/output \
+	$container \
+	braker.pl \
+	--genome /input/c_elegans.PRJNA13758.WS283.genome.fa \
+	--species Ce_N2 \
+	--prot_seq /input/N2_WS283.protein.fa \
+	--threads 24 \
+	--busco_lineage=nematoda_odb10 \
+	--gff3 \
+	--workingdir /output 
