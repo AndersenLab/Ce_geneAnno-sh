48414fdece0de33e4ce1baacceee4d0ad6c80703
lanceman999 <loconn13@jh.edu>
2026-01-22 15:31:22 -0500

    code to visualize potential chimeric contigs in Ce wild strain de novo assemblies

--- asm_gene_set/chimeric_contigs.R
+++ asm_gene_set/chimeric_contigs.R
@@ -2,23 +2,55 @@ library(ggplot2)
 library(readr)
 library(dplyr)
 
+soi <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/chimeric_contigs/master_strain_contigs.tsv", col_names = c("strain","contig")) %>%
+  dplyr::mutate(strain = ifelse(strain == "PB306", "ECA259", strain))
+strains <- soi %>% dplyr::distinct(strain) %>% dplyr::pull(strain)
 
-# MY1
-nuc <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/synteny_vis/elegans/nucmer_aln_WSs/142_nucmer_ECA741CGC1.tsv", col_names = c("N2S","N2E","WSS","WSE","L1","L2","IDY","LENR","LENQ","N2_chr","contig","strain")) %>%
-  dplyr::filter(strain == 'MY1') %>% dplyr::filter(N2_chr == "II" | N2_chr == "III" | N2_chr == "X", contig == "ptg000006l" | contig == "ptg000013l")
+nucmer <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/synteny_vis/elegans/nucmer_aln_WSs/142_nucmer_ECA741CGC1.tsv", col_names = c("N2S","N2E","WSS","WSE","L1","L2","IDY","LENR","LENQ","N2_chr","contig","strain")) 
 
-conservedINV <- ggplot(nuc) +
-  geom_segment(aes(x = N2S / 1e6, xend = N2E / 1e6, y = WSS / 1e6, yend = WSE / 1e6, color = contig), linewidth = 1) +
-  facet_wrap(~N2_chr, scales = "free") +
-  theme(
-    # legend.position = 'none',
-    axis.text = element_text(size = 14, color = 'black'),
-    axis.ticks = element_blank(),
-    axis.title = element_text(size = 16, color = 'black', face = 'bold'),
-    panel.background = element_blank(),
-    panel.grid = element_blank(),
-    panel.border = element_rect(fill = NA),
-    plot.title = element_text(size = 24, color = 'black', face = 'bold', hjust = 0.5)) +
-  # coord_cartesian(xlim = c(8.36, 8.38)) +
-  labs(x = "N2 genome position (Mb)", y = "Wild strain contig position (Mb)")
-conservedINV
+
+
+
+
+
+chimeric_plotting <- function(strain_of_interest) {
+  ctgs <- soi %>% dplyr::filter(strain == strain_of_interest) %>% dplyr::pull(contig)
+  
+  aln <- nucmer %>%
+      dplyr::filter(strain == strain_of_interest & contig %in% ctgs)
+  
+  plot_chim <- ggplot(aln) +
+    geom_segment(aes(x = N2S / 1e6, xend = N2E / 1e6, y = WSS / 1e6, yend = WSE / 1e6, color = contig), linewidth = 1) +
+    facet_wrap(~N2_chr, scales = "free") +
+    theme(
+      # legend.position = 'none',
+      axis.text = element_text(size = 14, color = 'black'),
+      axis.ticks = element_blank(),
+      axis.title = element_text(size = 16, color = 'black', face = 'bold'),
+      panel.background = element_blank(),
+      panel.grid = element_blank(),
+      panel.border = element_rect(fill = NA),
+      plot.title = element_text(size = 24, color = 'black', face = 'bold', hjust = 0.5)) +
+    # coord_cartesian(xlim = c(8.36, 8.38)) +
+    labs(x = "N2 genome position (Mb)", y = "Wild strain contig position (Mb)", title = paste0(strain_of_interest, " chimeric alignment"))
+  plot_chim
+}
+
+
+
+
+
+
+
+plots <- list()
+i=1
+for (strain in strains) {
+  print(paste0(strain, " ", i, "/", length(strains)))
+  i=i+1
+  plots[[strain]] <- chimeric_plotting(strain)
+}
+
+plots
+
+
+
