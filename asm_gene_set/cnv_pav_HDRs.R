de2a58db9ccd1226b740db0977ebbd7f73a16dcc
lanceman999 <loconn13@jh.edu>
2026-03-02 15:44:31 -0500

    updates with Wilcoxon test

--- asm_gene_set/cnv_pav_HDRs.R
+++ asm_gene_set/cnv_pav_HDRs.R
@@ -2,26 +2,31 @@ library(ggplot2)
 library(dplyr)
 library(data.table)
 library(readr)
+library(rstatix)
+library(ggpubr)
+
 
 ws_hdr_ogs <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/ws_HDR_liftover/hdr_genes_OG_class.tsv")
 strains <- ws_hdr_ogs %>% dplyr::distinct(strain) %>% dplyr::pull()
 all_relations <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/ws_HDR_liftover/OG_relations_matrix_count.tsv") %>%
   dplyr::rename_with(~ sub("_count$", "", .x))
+sc_ogs <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/assemblies/orthology/elegans/orthofinder/64_core/OrthoFinder/Results_Dec07/Orthogroups/Orthogroups_SingleCopyOrthologues.txt", col_names = "OGs") %>%
+  dplyr::pull(OGs)
   
+cnv_pav_results_df = as.data.frame(matrix(ncol = 11, nrow = 140))
+names(cnv_pav_results_df) = c("strain", "CNV_inHDR", "PAV_inHDR", "one_to_one_inHDR", "single_copy_OGs_inHDRs", "CNV_nonHDR", "PAV_nonHDR", "one_to_one_nonHDR", "single_copy_OGs_nonHDRs", "HDR_OG_count", "nonHDR_OG_count")
   
-cnv_pav_results_df = as.data.frame(matrix(ncol = 9, nrow = 140))
-names(cnv_pav_results_df) = c("strain", "CNV_inHDR", "PAV_inHDR", "one_to_one_inHDR", "CNV_nonHDR", "PAV_nonHDR", "one_to_one_nonHDR", "HDR_OG_count", "nonHDR_OG_count")
-  
+# Calculating stats for every wild strain and appending to a master dataframe
 for (i in 1:length(strains)) {
   soi <- strains[i]
   print(paste0("On strain: ", soi, ". ", i, "/140."))
   
   cnv_pav_results_df[i,1] = c(soi)
   
+  # HDRs stats
   hdr_ogs <- ws_hdr_ogs %>% dplyr::filter(strain == soi) %>% dplyr::distinct(Orthogroup) %>% dplyr::pull()
-  # print(length(hdr_ogs))
-  cnv_pav_results_df[i,8] = c(length(hdr_ogs))
-  
+  cnv_pav_results_df[i,10] = c(length(hdr_ogs))
+
   hdr_og_relations <- all_relations %>% dplyr::select(Orthogroup, N2, dplyr::all_of(soi)) %>% dplyr::filter(Orthogroup %in% hdr_ogs) %>% filter(!(is.na(N2) & is.na(.data[[soi]]))) %>%
     dplyr::mutate(
       relation = dplyr::case_when(
@@ -30,8 +35,8 @@ for (i in 1:length(strains)) {
         TRUE                            ~ "CNV" )) %>%
     dplyr::count(relation, name = "count")
   
-  # print(hdr_og_relations %>% dplyr::distinct())
-  
+  sg_ogs <- all_relations %>% dplyr::filter(Orthogroup %in% hdr_ogs) %>% dplyr::filter(Orthogroup %in% sc_ogs) %>% dplyr::distinct(Orthogroup) %>% dplyr::pull()
+
   cnv_hdr <- hdr_og_relations %>% dplyr::filter(relation == "CNV") %>% dplyr::pull(count)
   pav_hdr <- hdr_og_relations %>% dplyr::filter(relation == "PAV") %>% dplyr::pull(count)
   one_to_one <- hdr_og_relations %>% dplyr::filter(relation == "one_to_one") %>% dplyr::pull(count)
@@ -39,8 +44,10 @@ for (i in 1:length(strains)) {
   cnv_pav_results_df[i,2] = c(cnv_hdr)
   cnv_pav_results_df[i,3] = c(pav_hdr)
   cnv_pav_results_df[i,4] = c(one_to_one)
+  cnv_pav_results_df[i,5] = c(length(sg_ogs))
   
   
+  # outside of HDRs stats
   nonhdr_og_relations <- all_relations %>% dplyr::select(Orthogroup, N2, soi) %>% dplyr::filter(!Orthogroup %in% hdr_ogs) %>% filter(!(is.na(N2) & is.na(.data[[soi]]))) %>%
     dplyr::mutate(
       relation = dplyr::case_when(
@@ -50,18 +57,151 @@ for (i in 1:length(strains)) {
     dplyr::count(relation, name = "count")
   
   nonhdr_ogs <- all_relations %>% dplyr::select(Orthogroup, N2, soi) %>% dplyr::filter(!Orthogroup %in% hdr_ogs) %>% filter(!(is.na(N2) & is.na(.data[[soi]])))
+  sg_ogs_nonHDR <- all_relations %>% dplyr::filter(!Orthogroup %in% hdr_ogs) %>% dplyr::filter(Orthogroup %in% sc_ogs) %>% dplyr::distinct(Orthogroup) %>% dplyr::pull()
   
-  cnv_pav_results_df[i,9] = c(nrow(nonhdr_ogs))
+  
+  cnv_pav_results_df[i,11] = c(nrow(nonhdr_ogs))
   
   cnv_hdr <- nonhdr_og_relations %>% dplyr::filter(relation == "CNV") %>% dplyr::pull(count)
   pav_hdr <- nonhdr_og_relations %>% dplyr::filter(relation == "PAV") %>% dplyr::pull(count)
   one_to_one <- nonhdr_og_relations %>% dplyr::filter(relation == "one_to_one") %>% dplyr::pull(count)
   
-  cnv_pav_results_df[i,5] = c(cnv_hdr)
-  cnv_pav_results_df[i,6] = c(pav_hdr)
-  cnv_pav_results_df[i,7] = c(one_to_one)
+  cnv_pav_results_df[i,6] = c(cnv_hdr)
+  cnv_pav_results_df[i,7] = c(pav_hdr)
+  cnv_pav_results_df[i,8] = c(one_to_one)
+  cnv_pav_results_df[i,9] = c(length(sg_ogs_nonHDR))
+  
 }
 
 
-# non-normalized results!
 
+# non-normalized results plotted
+plot_df <- cnv_pav_results_df %>%
+  tidyr::pivot_longer(
+    cols = -strain,
+    names_to = "metric",
+    values_to = "value") %>%
+  dplyr::mutate(region = case_when(
+      stringr::str_detect(metric, "nonHDR") ~ "non-HDR",
+      stringr::str_detect(metric, "HDR") ~ "HDR",
+      TRUE ~ NA)) %>%
+  dplyr::filter(!is.na(region)) %>%
+  dplyr::mutate(region = factor(region, levels = c("HDR", "non-HDR")),
+                metric = ifelse(metric == "HDR_OG_count", "OG_count", 
+                                ifelse(metric == "nonHDR_OG_count", "OG_count", metric)),
+                stat = metric %>%
+                  stringr::str_remove("_inHDRs?$") %>%
+                  stringr::str_remove("_nonHDRs?$"),
+                stat = ifelse(stat == "one_to_one","one-to-one", 
+                              ifelse(stat == "single_copy_OGs", "single-copy OGs",
+                                     ifelse(stat == "OG_count", "OGs", stat))),
+                stat = factor(stat, levels = c("CNV","PAV","one-to-one","single-copy OGs","OGs")))
+
+ggplot(plot_df, aes(x = stat, y = value, fill = region)) +
+  geom_boxplot(outlier.size = 0.6, width = 0.7, position = position_dodge(width = 0.75), outlier.shape = NA, alpha = 0.5) +
+  geom_point(position = position_jitterdodge(jitter.width = 0.5, dodge.width = 0.75), size = 1.25) +
+  scale_fill_manual(values = c("HDR" = "red", "non-HDR" = "blue")) +
+  theme_bw() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(size = 22, color = 'black', face = 'bold'),
+    panel.grid.major.x = element_blank(),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 20, color = 'black'),
+    axis.text.y = element_text(size = 18, color = 'black'),
+    axis.title.y = element_text(size = 22, color = 'black', face = 'bold')
+  ) +
+  scale_y_log10() +
+  labs(y = "Orthogroup count", fill = "Region")
+
+
+# normalizing for # of orthogroups in HDRs versus not in HDRs
+plot_df_norm <- cnv_pav_results_df %>%
+  dplyr::rename(inHDR_OG_count = HDR_OG_count) %>%
+  dplyr::mutate(
+    dplyr::across(dplyr::matches("inHDR"), ~ .x / inHDR_OG_count),
+    dplyr::across(dplyr::matches("nonHDR"), ~ .x / nonHDR_OG_count)) %>%
+  tidyr::pivot_longer(
+    cols = -strain,
+    names_to = "metric",
+    values_to = "value") %>%
+  dplyr::mutate(region = case_when(
+    stringr::str_detect(metric, "nonHDR") ~ "non-HDR",
+    stringr::str_detect(metric, "HDR") ~ "HDR",
+    TRUE ~ NA)) %>%
+  dplyr::filter(!is.na(region)) %>%
+  dplyr::mutate(region = factor(region, levels = c("HDR", "non-HDR")),
+                metric = ifelse(metric == "inHDR_OG_count", "OG_count", 
+                                ifelse(metric == "nonHDR_OG_count", "OG_count", metric)),
+                stat = metric %>%
+                  stringr::str_remove("_inHDRs?$") %>%
+                  stringr::str_remove("_nonHDRs?$"),
+                stat = ifelse(stat == "one_to_one","one-to-one", 
+                              ifelse(stat == "single_copy_OGs", "single-copy OGs",
+                                     ifelse(stat == "OG_count", "OGs", stat))),
+                stat = factor(stat, levels = c("CNV","PAV","one-to-one","single-copy OGs","OGs"))) %>%
+  dplyr::filter(stat != "OGs")
+
+
+ggplot(plot_df_norm, aes(x = stat, y = value, fill = region)) +
+  geom_boxplot(outlier.size = 0.6, width = 0.7, position = position_dodge(width = 0.75), outlier.shape = NA, alpha = 0.5) +
+  geom_point(position = position_jitterdodge(jitter.width = 0.5, dodge.width = 0.75), size = 1.25) +
+  scale_fill_manual(values = c("HDR" = "red", "non-HDR" = "blue")) +
+  theme_bw() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(size = 26, color = 'black', face = 'bold'),
+    panel.grid.major.x = element_blank(),
+    legend.box.background = element_rect(color = "black", size = 1),
+    legend.position  = 'inside',
+    legend.position.inside = c(0.945,0.945),
+    legend.title = element_blank(),
+    legend.key.size = unit(1.5, "cm"),
+    legend.text = element_text(size = 24, color = 'black'),
+    axis.text.y = element_text(size = 18, color = 'black'),
+    axis.title.y = element_text(size = 26, color = 'black', face = 'bold')
+  ) +
+  scale_y_log10() +
+  labs(y = "Orthogroup count", fill = "Region")
+
+
+
+
+
+# Calculating stats and adding to plot:
+
+# Paired Wilcoxon signed-rank test
+wilcox_results <- plot_df_norm %>%
+  group_by(stat) %>%
+  wilcox_test(value ~ region, paired = TRUE) %>%
+  adjust_pvalue(method = "BH") %>% # Benjamini-Hochberg correction
+  add_significance()
+
+y_pos <- plot_df_norm %>%
+  group_by(stat) %>%
+  summarise(y.position = max(value, na.rm = TRUE) * 1.3, .groups = "drop")
+
+wilcox_results <- wilcox_results %>%
+  left_join(y_pos, by = "stat")
+
+ggplot(plot_df_norm, aes(x = stat, y = value, fill = region)) +
+  geom_boxplot(outlier.size = 0.6, width = 0.7, position = position_dodge(width = 0.75), outlier.shape = NA, alpha = 0.5) +
+  geom_point(position = position_jitterdodge(jitter.width = 0.5, dodge.width = 0.75), size = 1.25) +
+  scale_fill_manual(values = c("HDR" = "red", "non-HDR" = "blue")) +
+  stat_pvalue_manual(wilcox_results, label = "p.adj.signif", x = "stat", y.position = "y.position", inherit.aes = FALSE) +
+  scale_y_log10() +
+  labs(y = "Orthogroup count", fill = "Region") +
+  theme_bw() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(size = 26, color = 'black', face = 'bold'),
+    panel.grid.major.x = element_blank(),
+    legend.box.background = element_rect(color = "black", size = 1),
+    legend.position  = 'inside',
+    legend.position.inside = c(0.945,0.945),
+    legend.title = element_blank(),
+    legend.key.size = unit(1.5, "cm"),
+    legend.text = element_text(size = 24, color = 'black'),
+    axis.text.y = element_text(size = 18, color = 'black'),
+    axis.title.y = element_text(size = 26, color = 'black', face = 'bold')
+  ) 
