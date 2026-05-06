282faf75e0fa3193d0fac0656db6c1c1996df317
lanceman999 <loconn13@jh.edu>
2026-05-05 20:21:10 -0400

    script added to run SyRI

---
+++ asm_gene_set/syri_CB4856.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -J syri
+#SBATCH -A eande106
+#SBATCH -p parallel
+#SBATCH -t 48:00:00
+#SBATCH -N 1
+#SBATCH -c 48
+
+source activate SyRI
+
+REF="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/SyRI/genomes/N2.PRJNA13758.WS283.genome.fa"
+QUERY="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/SyRI/genomes/CB4856.PRJNA275000.WS272.chromIDsFixed.fa"
+ALN="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/SyRI/genomes/out_i90_l100.CB4856.coords"
+DELTA="/vast/eande106/projects/Lance/THESIS_WORK/gene_annotation/processed_data/SyRI/genomes/out_i90_l100.CB4856.delta
+"
+syri -c $ALN -F T -d -r $REF -q $QUERY --samplename CB4856_syri
